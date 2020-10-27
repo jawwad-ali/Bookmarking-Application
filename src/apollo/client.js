ef2e717e1c4335492ca0fa44c5ed437dba56689e
jawwad-ali <alijawwad001@gmail.com>
2020-10-27 19:41:20 +0500

    update production url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
     link: new HttpLink({
-        uri: '/.netlify/functions/bookmark',
+        uri: 'https://reverent-easley-afe34b.netlify.app/.netlify/functions/bookmark',
         fetch,
     }),
     cache: new InMemoryCache()
