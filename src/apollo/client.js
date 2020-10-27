be91b350b200a08fe1036e3b9e4cb7ba8304d2e5
jawwad-ali <alijawwad001@gmail.com>
2020-10-27 19:57:27 +0500

    set url to deploy site

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
     link: new HttpLink({
-        uri: 'https://reverent-easley-afe34b.netlify.app/.netlify/functions/bookmark',
+        uri: '/.netlify/functions/bookmark',
         fetch,
     }),
     cache: new InMemoryCache()
